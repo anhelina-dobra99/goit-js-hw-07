10419ea1b0eab28f009e2dbbcc2cb7106bcc895a
Anhelina Dobra <dobraangelina@gmail.com>
2025-01-19 22:47:26 +0100

    do task2

--- js/task-2.js
+++ js/task-2.js
@@ -24,3 +24,14 @@ const images = [
     alt: "Lighthouse Coast Sea",
   }
 ];
+
+const gallery = document.querySelector(".gallery");
+images.forEach(image => {
+  const li = document.createElement("li");
+  const img = document.createElement("img");
+  img.src = image.url;
+  img.alt = image.alt;
+  li.appendChild(img);
+  gallery.appendChild(li);
+
+});
