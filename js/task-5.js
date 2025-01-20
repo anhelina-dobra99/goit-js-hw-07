ca0839e4a992518491e6b59bf8b55d74b9f9b491
Anhelina Dobra <dobraangelina@gmail.com>
2025-01-20 13:18:09 +0100

    do tasks4-6

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,14 @@
-function getRandomHexColor() {
+const changeColorButton = document.querySelector(".change-color");
+const body = document.querySelector("body");
+const span = document.querySelector(".color");
+function changeColor(event) {
+  function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+  const currentColor = body.style.backgroundColor = getRandomHexColor();
+  span.textContent = currentColor;
+}
+changeColorButton.addEventListener("click", changeColor);
+
--- js/task-6.js
+++ js/task-6.js
@@ -1,3 +1,29 @@
+const numberBox = document.querySelector(".number-box");
+const createButton = document.querySelector("[data-create]");
+const destroyButton = document.querySelector("[data-destroy]");
+const boxesContainer = document.querySelector("#boxes");
+createButton.addEventListener("click", event => {
+  const boxCount = parseInt(numberBox.value);
+  if (boxCount >= 1 && boxCount <= 100) {
+     boxesContainer.innerHTML = ""; 
+    createBoxes(boxCount);
+  }
+  numberBox.value = "";
+});
+destroyButton.addEventListener("click", event => {
+  boxesContainer.innerHTML = "";
+});
+function createBoxes(boxCount) {
+  const boxes = [];
+  for (let i = 0; i < boxCount; i++) {
+    const box = document.createElement("div");
+    box.style.width = `${30 + i * 10}px`;
+    box.style.height = `${30 + i * 10}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxes.push(box);
+  }
+  boxesContainer.append(...boxes);
+}
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
